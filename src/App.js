b580223781fd7538cbb0fd3e9167a0df0ffb235d
Tanya-Srivastava <tanyasrivastava2808@gmail.com>
2024-06-26 23:54:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './customer/component/navigation/navigation';
+
+function App() {
+  return (
+    <div className="">
+      <Navigation/>
+      <div>
+        home
+      </div>
+    </div>
+  );
+}
+
+export default App;
